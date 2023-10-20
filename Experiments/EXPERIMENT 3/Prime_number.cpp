39d7a60907013a5755bf1256dd73237558076f6b
Sanjay Rajan <91653459+Sanjayrajanrajappa@users.noreply.github.com>
2023-10-20 14:33:50 +0530

    Prime_number.cpp

--- Experiments/EXPERIMENT 3/Prime_number.cpp
+++ Experiments/EXPERIMENT 3/Prime_number.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+    int i, n; 
+    bool is_prime = true; 
+    cout<<"Enter a positive integer: "; 
+    cin>>n; 
+    if (n == 0 || n == 1) { 
+        is_prime = false; 
+    } 
+    for (i = 2; i <= n/2; ++i) { 
+        if (n % i == 0) { 
+            is_prime = false; 
+            break; 
+        } 
+    }
+    if (is_prime){
+        cout<<"The number "<<n<<" is a prime number"; 
+    }
+    else{
+        cout<<"The number "<<n<< " is not a prime number"; 
+    } 
+    cout<<"\nDone By SANJAY RAJAN R CSE D";
+    cout<<"\nREG NO. 23CS210";
+    return 0;    
+}
