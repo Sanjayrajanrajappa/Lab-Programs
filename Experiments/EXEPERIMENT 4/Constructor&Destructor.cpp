141170f94408cb3191d4db85d62814f9b1c2d8e6
Sanjay Rajan <91653459+Sanjayrajanrajappa@users.noreply.github.com>
2023-10-27 16:17:07 +0530

    Constructor&Destructor.cpp

--- Experiments/EXEPERIMENT 4/Constructor&Destructor.cpp
+++ Experiments/EXEPERIMENT 4/Constructor&Destructor.cpp
@@ -10,6 +10,8 @@ public:
     ~Simpleclass() 
     { 
         cout<<"Destructor is called!"<<endl;
+        cout<<"\nDone By SANJAY RAJAN R CSE D";
+        cout<<"\nREG NO. 23CS210";
     } 
 }; 
 int main() 
@@ -17,7 +19,5 @@ int main()
     cout<<"Creating an instance of a Simpleclass..."<<endl; 
     Simpleclass obj;
     cout<<"Doing some work with a Simpleclass..."<<endl; 
-    cout<<"\nDone By SANJAY RAJAN R CSE D";
-    cout<<"\nREG NO. 23CS210";
     return 0; 
 }
