6b8ea5e51f3576b7946bb72267bab82e094621bb
Sanjay Rajan <91653459+Sanjayrajanrajappa@users.noreply.github.com>
2023-10-27 19:10:11 +0530

    Polymorphism.cpp

--- Experiments/EXPERIMENT 8/Polymorphism.cpp
+++ Experiments/EXPERIMENT 8/Polymorphism.cpp
@@ -1,1 +1,39 @@
-
+#include <iostream> 
+using namespace std;
+class Shape 
+{ 
+public: 
+    virtual void draw()
+    { 
+        cout<<"Drawing a Shape...."<<endl; 
+    } 
+}; 
+class Circle : public Shape
+{ 
+public: 
+    void draw() override
+    { 
+        cout<<"Drawing a Circle...."<<endl; 
+    } 
+}; 
+class Square : public Shape 
+{ 
+public: 
+    void draw() override
+    { 
+        cout<<"Drawing a Square....."<<endl; 
+    } 
+}; 
+int main() 
+{ 
+    Shape* shapePtr;  
+    Circle circle; 
+    Square square; 
+    shapePtr = &circle; 
+    shapePtr->draw();  
+    shapePtr = &square; 
+    shapePtr->draw();
+    cout<<"\nDone By SANJAY RAJAN R CSE D";
+    cout<<"\nREG NO. 23CS210";
+    return 0; 
+}
